6e401fbffef848c756a1b516aca4944621321a70
Tom Goring <tgoring97@gmail.com>
2019-08-21 18:52:18 +0100

    Start over again

--- Portfolio/scripts.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello");
-var divs = document.getElementsByClassName('dummy');
-for (var i = 0; i < divs.length; i++) {
-    var color = '#'+Math.floor(Math.random()*16777215).toString(16);
-    var colorString = '0px 0px 30px 0px ' + color;
-    console.log(color)
-    divs[i].style['background-color'] = color;
-}
