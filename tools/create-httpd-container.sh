2c84bb9292734947c0e07f00750721ac4a359981
Tom Goring <tgoring97@gmail.com>
2019-08-27 23:01:43 +0000

    Update README.me, try to fix broken images, sort of update docker run script (but it's broken)

--- tools/create-httpd-container.sh
+++ tools/create-httpd-container.sh
@@ -1,5 +1,9 @@
+cd ~/www.tomgoring.co.uk/
+docker build -t httpd-docker .
 docker run -dit --name httpd -p 8080:80 \
-	-v ~/website/src/:/usr/local/apache2/htdocs/ \
-	-v ~/website/images/:/usr/local/apache2/images/ \
-	-v ~/website/icons/:/usr/local/apache2/icons/ \
+	-v ~/www.tomgoring.co.uk/src/:/usr/local/apache2/htdocs/ \
+	-v ~/www.tomgoring.co.uk/images/:/usr/local/apache2/images/ \
+	-v ~/www.tomgoring.co.uk/icons/:/usr/local/apache2/icons/ \
+	-v ~/www.tomgoring.co.uk/config/:/usr/local/apache2/conf/ \
 	httpd-docker
+cd -
