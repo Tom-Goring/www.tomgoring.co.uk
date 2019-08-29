f4421aea2e0d95d185168a36abb703487bd37901
Tom Goring <tgoring97@gmail.com>
2019-08-29 23:23:59 +0100

    Add a nifty sort of animation (I don't know how I did this)

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,6 +1,8 @@
-$(function(){
-    let lines = $('#title-box h1');
-    lines.each(function(i) {
-        $(this).delay(i * 1000).fadeIn(2000);
-    })
+$(function() {
+    let rows = $('#title-box .row');
+    rows.hide();
+
+    rows.each(function(i) {
+        $(this).delay(1500 * i).show(1000);
+    });
 });
