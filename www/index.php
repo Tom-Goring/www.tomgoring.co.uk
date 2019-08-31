126901876790d2ac5371501ba700079ee8b3fe10
Tom Goring <tgoring97@gmail.com>
2019-08-31 21:30:57 +0100

    Clean up a bit again

--- www/index.php
+++ www/index.php
@@ -4,11 +4,23 @@
     <title>Tom Goring | Intern Software Engineer</title>
     <?php include ('html/header.html'); ?>
 </head>
-<body>
-
+<body class="flex">
 
 <canvas id="canvas" width="800" height="800"></canvas>
-<div class=""></div>
+
+<div class="flex on-top" id="hero-page">
+
+    <div class="text">
+        Hello, I'm
+        <span class="highlight">Tom Goring</span>.
+        <br>
+        <span class="sub-text">Software Engineer Intern</span>
+    </div>
+    <button>
+        <span>Find out more</span><i class="fas fa-arrow-right"></i>
+    </button>
+
+</div>
 
 
 <script src="js/canvas.js"></script>
