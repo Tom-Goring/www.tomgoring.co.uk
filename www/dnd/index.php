79ae59fd012ea14be4cffc07396dde413e5ba850
Tom Goring <tgoring97@gmail.com>
2019-09-01 20:50:31 +0100

    Add section for the D&D character creator I've planned

---
+++ www/dnd/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tom Goring | Intern Software Engineer</title>
+    <?php include ('../html/header.html'); ?>
+</head>
+<body>
+
+<section>
+    <h1>HELLO</h1>
+</section>
+
+<?php include ('../html/footer.html'); ?>
+</body>
+</html>
