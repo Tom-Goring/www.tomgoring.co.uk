1077ea96093a3c00136d7e302d1ce4fd3093f9c9
Tom Goring <tgoring97@gmail.com>
2019-09-08 17:29:02 +0100

    Add initial frame for contact me backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,36 @@
-var express = require('express');
-var app = express();
-var port = process.env.port || 3000;
+require('dotenv').config();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+const port = process.env.port || 3000;
  
 app.get("/contactMe",function(request,response)
 {
-    response.json({"Message":"Welcome to Node js"});
+    let mailOpts, smtpTrans;
+    smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS
+        }
+    });
+    mailOpts = {
+        from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+        to: process.env.GMAIL_USER,
+        subject: 'New message from contact form at tomgoring.co.uk',
+        text: `${req.body.name} (${req.body.email}) says :${req.body.message}`
+    };
+    smtpTrans.sendMail(mailOpts, function (error, response) {
+        if (error) {
+            res.render('contact-failure');
+        } else {
+            res.render('contact-success');
+        }
+    });
 });
  
 app.listen(port, function () {
