c3691a70d9f620dd54cac3a78212b9caab3c10cd
Tom Goring <tgoring97@gmail.com>
2019-09-08 16:01:54 +0000

    Add API subdomain and initial nodeJS API handler

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.port || 3000;
+ 
+app.get("/contactMe",function(request,response)
+{
+    response.json({"Message":"Welcome to Node js"});
+});
+ 
+app.listen(port, function () {
+    var datetime = new Date();
+    var message = "Server runnning on Port:- " + port + "Started at :- " + datetime;
+    console.log(message);
+});
